0e4f092958339f8f7c7c9bb47ffbfbc021efebab
rayna williams <Raynawilliams123@gmail.com>
2019-12-17 16:24:28 -0700

    origin.master

--- script.js
+++ script.js
@@ -2,13 +2,32 @@
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
-
+var startButton = document.getElementById("start")
+var sec = 15;
+var time = setInterval(myTimer, 1000);
 
 
 //functions
-function buildQuiz(){}
+startButton.addEventListener('click', myTimer);
 
-function showResults(){}
+
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+    }
+}
+function buildQuiz() {
+    var output = [];
+}
+//for each question
+myQuestions.forEach(
+    ()
+)
+
+function showResults() { }
 
 // display quiz right away
 buildQuiz();
