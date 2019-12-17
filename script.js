ee8397df352c8d3ca94a4362ce1429339a6945b2
rayna williams <Raynawilliams123@gmail.com>
2019-12-16 17:57:40 -0700

    origin.master

---
+++ script.js
@@ -0,0 +1,17 @@
+//variables
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+
+
+//functions
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
