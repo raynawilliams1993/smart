64ddb81d2eb47369393b9cab19621109b2ef15f3
rayna williams <Raynawilliams123@gmail.com>
2019-12-17 17:57:13 -0700

    origin.master

--- script.js
+++ script.js
@@ -2,30 +2,67 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var questions = document.getElementById("question");
-var qImg = document.getElementById("qImg");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
 var progress = document.getElementById("progress");
+var submit =document.getElementById("submit");
 var scoreDiv = document.getElementById("scoreContainer");
-var lastQuestion = questions.length - 1;
-var runningQuestion = 0;
+var lastQuestionIndex = questions.length - 1;
+var runningQuestionIndex = 0;
+var questionTime= 15;
+var gaugeWidth= 150;
+var count= 0;
+var gaugeProgressUnit= gaugeWidth/questionTime;
 
 
 //functions
-function renderQuestion(){
-    var q = questions[runningQuestion];
-    questions.innerHTML = "<p>" + q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+
+
+function renderQuestion() {
+    var q = questions[runningQuestionIndex];
+    question.innerHTML = "<p>" + q.question +"</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
     choiceD.innerHTML = q.choiceD;
 }
-function renderProgress(){
-    for(var qIndex = 0; qIndex <= lastQuestion; qIndex++){
+function renderProgress() {
+    for(var qIndex = 0; qIndex <= lastQuestionIndex; qIndex++){
         progress.innerHTML += "<div class = 'prog' id="+ qIndex +"></div>";
     }
 }
+function answerIsCorrect() {
+    document.getElementById(runningQuestionIndex).style.backgroundColor= "green";
+}
+function answerIsWrong() {
+    document.getElementById(runningQuestionIndex).style.backgroundColor= "red";
+}
+function counterRender() {
+    if(count <= questionTime) {
+        counter.innerHTML = count;
+        timeGauge.style.width= gaugeProgressUnit * count + "px";
+        count ++;
+    }else{
+       count= 0;
+       answerIsWrong();
+       
+    }
+}
+
+
+
+
+//buttons
+start.addEventListener("click", startQuiz);
+
+
+//quiz
+start.style.display= "none";
+renderQuestion();
+quiz.style.display = "block";
+renderProgress();
+renderCounter();
+TIMER = setInterval(renderCounter,1500);
